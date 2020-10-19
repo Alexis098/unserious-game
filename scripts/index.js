73ec939a3ecd7ed32fdcccf2025f4dab9f599723
davidmarsprof <72275002+davidmarsprof@users.noreply.github.com>
2020-10-19 11:44:37 +0200

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,5 @@
-/**
- * index.js
- * - All our useful JS goes here, awesome!
- */
-
-console.log("JavaScript is amazing!");
-
-
-//todo qui veut ;)
-// S'inspirer du script ci-dessous pour faire un décompte "temps restant" et afficher perdu si on ne répond pas dans un délais correct.
+console.log("c'est ici qu'il y a une enigme");
+// S'inspirer du script ci-dessous pour faire un décompte "temps restant" et afficher perdu si on ne répond pas en moins de 15 secondes.
 
 function startChrono(){
     let time=0;
@@ -15,6 +7,10 @@ function startChrono(){
         function(){
             time++; // 1,2,3,4 etc...
             document.title="ça fait "+time+" secondes que tu réfléchis"; //affiche le temps passé dans le titre de page
+            // todo afficher le décompte dans la page HTML
+            // todo Quand ça fait 5 secondes on affiche "dépeche toi" 
+            // todo Quand ça fait 10 secondes on affiche "ça commence à être chaud" 
+            // todo Quand ça fait 15 secondes on affiche "trop tard" pendant 2 secondes, puis on redirige vers la (bonne) page "perdu" 
         },
     1000 // toutes les 1000 millisecondes
     );
